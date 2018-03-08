39501235f23e707f5c457f45c0b6a3169ab9bbec
letusfly85 <letusfly95@gmail.com>
2018-03-08 23:27:53 +0900

    refactor: remove unused class

--- app/io/wonder/soft/example/application/workflow/service/TaskService.scala
+++ app/io/wonder/soft/example/application/workflow/service/TaskService.scala
@@ -2,6 +2,6 @@ package io.wonder.soft.example.application.workflow.service
 
 import io.wonder.soft.example.application.workflow.ApplicationService
 
-class TaskService extends ApplicationService {
+object TaskService extends ApplicationService {
 
 }
