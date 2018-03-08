3b0d5960c8e7b44413552e23d19d044e05443d40
letusfly85 <letusfly95@gmail.com>
2018-03-08 16:08:43 +0900

    style: use query DSL for default values

--- app/io/wonder/soft/example/domain/workflow/WorkflowStatusRepository.scala
+++ app/io/wonder/soft/example/domain/workflow/WorkflowStatusRepository.scala
@@ -3,12 +3,14 @@ package io.wonder.soft.example.domain.workflow
 import io.wonder.soft.example.domain.workflow.entity.WorkflowStatusEntity
 import io.wonder.soft.example.domain.{Entity, Repository}
 import io.wonder.soft.example.domain.workflow.model.WorkflowStatuses
+import scalikejdbc._
 
 import scala.util.{Failure, Success, Try}
 
 object WorkflowStatusRepository extends Repository {
   import WorkflowStatusEntity._
 
+  val wsc = WorkflowStatuses.column
   override def find(id: Int): Option[WorkflowStatusEntity] = {
     WorkflowStatuses.find(id) match {
       case Some(status) => Some(status)
@@ -19,7 +21,13 @@ object WorkflowStatusRepository extends Repository {
   override def create(entity: Entity): Either[Throwable, WorkflowStatusEntity] = {
     val status = entity.asInstanceOf[WorkflowStatusEntity]
     Try {
-      WorkflowStatuses.create(name = status.name).save()
+      DB localTx {implicit session =>
+        withSQL {
+          insert.into(WorkflowStatuses).namedValues(
+            wsc.name -> status.name
+          )
+        }.update().apply()
+      }
 
     } match {
       case Success(_) => Right(status)
