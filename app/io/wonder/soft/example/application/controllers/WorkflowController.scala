f1039eede6971f2fd13558f0b0d12bbc671f3f3c
letusfly85 <letusfly95@gmail.com>
2018-03-08 15:59:46 +0900

    style: not use repository directory

--- app/io/wonder/soft/example/application/controllers/WorkflowController.scala
+++ app/io/wonder/soft/example/application/controllers/WorkflowController.scala
@@ -2,9 +2,9 @@ package io.wonder.soft.example.application.controllers
 
 import javax.inject._
 
-import com.google.gson.JsonObject
+import io.wonder.soft.example.application.services.WorkflowService
 import io.wonder.soft.example.domain.workflow.entity.WorkflowStatusEntity
-import io.wonder.soft.example.domain.workflow.{WorkflowFactory, WorkflowQueryProcessor, WorkflowStatusRepository}
+import io.wonder.soft.example.domain.workflow.{WorkflowFactory, WorkflowQueryProcessor}
 import play.api.Logger
 import play.api.mvc._
 import play.api.libs.json._
@@ -17,7 +17,7 @@ class WorkflowController @Inject()(cc: ControllerComponents) extends AbstractCon
       case Some(json) =>
         Json.fromJson[WorkflowStatusEntity](json) match {
           case JsSuccess(statusEntity, _) =>
-            WorkflowStatusRepository.create(statusEntity) match {
+            WorkflowService.createStatus(statusEntity) match {
               case Right(_) => Created(Json.toJson(statusEntity))
               case Left(e) =>
                 Logger.info(e.toString())
