f8c3d22def71fe5f9a4fc24da06c21163aceedff
letusfly85 <letusfly95@gmail.com>
2018-03-13 07:53:42 +0900

    feat: add query command of workflow_transisions

--- app/io/wonder/soft/example/domain/workflow/WorkflowQueryProcessor.scala
+++ app/io/wonder/soft/example/domain/workflow/WorkflowQueryProcessor.scala
@@ -1,7 +1,7 @@
 package io.wonder.soft.example.domain.workflow
 
 import io.wonder.soft.example.domain.workflow.entity._
-import io.wonder.soft.example.domain.workflow.model.{WorkflowDefinitions, WorkflowStatuses}
+import io.wonder.soft.example.domain.workflow.model.{WorkflowDefinitions, WorkflowStatuses, WorkflowTransitions}
 import scalikejdbc._
 
 object WorkflowQueryProcessor {
@@ -9,6 +9,7 @@ object WorkflowQueryProcessor {
 
   val wsm = WorkflowDefinitions.syntax("wsm")
   val wss = WorkflowStatuses.syntax("wss")
+  val wts = WorkflowTransitions.syntax("wtc")
 
   def searchStatuses(): List[WorkflowStatusEntity] = {
     WorkflowStatuses.findAll()
@@ -23,7 +24,7 @@ object WorkflowQueryProcessor {
           .where(sqls.eq(wsm.workflowId, workflowId))
       }.map(res => (WorkflowDefinitions(wsm)(res), WorkflowStatuses(wss)(res))).list().apply()
     }).map{ case (scheme: WorkflowDefinitions, status: WorkflowStatuses) =>
-      WorkflowFactory.createDefinitionEntity(scheme, status)
+      WorkflowFactory.buildDefinitionEntity(scheme, status)
     }
   }
 
@@ -36,8 +37,11 @@ object WorkflowQueryProcessor {
           .where(sqls.eq(wsm.id, id))
       }.map(res => (WorkflowDefinitions(wsm)(res), WorkflowStatuses(wss)(res))).single().apply()
     }).map{ case (scheme: WorkflowDefinitions, status: WorkflowStatuses) =>
-      WorkflowFactory.createDefinitionEntity(scheme, status)
+      WorkflowFactory.buildDefinitionEntity(scheme, status)
     }
   }
+
+  def searchTransitions(workflowId: Int): List[WorkflowTransitionEntity] =
+    WorkflowTransitions.findAllBy(sqls.eq(wts.workflowId, workflowId))
 
 }
