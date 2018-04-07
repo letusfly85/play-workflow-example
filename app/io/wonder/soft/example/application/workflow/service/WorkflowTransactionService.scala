b9f4e71eb1b7410709eadb44b127d01f7162f5fb
letusfly85 <letusfly95@gmail.com>
2018-04-07 09:40:27 +0900

    feat: remove Try

--- app/io/wonder/soft/example/application/workflow/service/WorkflowTransactionService.scala
+++ app/io/wonder/soft/example/application/workflow/service/WorkflowTransactionService.scala
@@ -4,10 +4,11 @@ import io.wonder.soft.example.application.ApplicationService
 import io.wonder.soft.example.domain.workflow.entity.WorkflowTransactionEntity
 import io.wonder.soft.example.domain.workflow.factory.WorkflowTransactionFactory
 import io.wonder.soft.example.domain.workflow.query.WorkflowQueryProcessor
-import io.wonder.soft.example.domain.workflow.repository.{WorkflowDefinitionRepository, WorkflowTransactionRepository}
+import io.wonder.soft.example.domain.workflow.repository.{
+  WorkflowDefinitionRepository,
+  WorkflowTransactionRepository
+}
 import javax.inject.Inject
-
-import scala.util.{Failure, Success, Try}
 
 class WorkflowTransactionService @Inject()(
     definitionRepository: WorkflowDefinitionRepository,
@@ -19,31 +20,26 @@ class WorkflowTransactionService @Inject()(
   def initialize(
       userId: String,
       workflowId: Int): Either[Exception, WorkflowTransactionEntity] = {
-    Try {
-      //todo check exists transaction
+    //todo check exists transaction
 
-      //find workflow definition
-      val defines = defineQuery.searchDefinitions(workflowId)
+    //find workflow definition
+    val defines = defineQuery.searchDefinitions(workflowId)
+    val maybeInitialDefine = defines.filter(d => d.isFirstStep).headOption
+    maybeInitialDefine match {
+      case Some(define) =>
+        //generate transaction id
+        val transactionId = java.util.UUID.randomUUID().toString
 
-      val maybeInitialDefine = defines.filter(d => d.isFirstStep).headOption
+        val transaction =
+          WorkflowTransactionFactory.build(userId, transactionId, define)
+        val initialTransaction = transaction.copy(isInit = true)
 
-      maybeInitialDefine match {
-        case Some(define) =>
-          //generate transaction id
-          val transactionId = java.util.UUID.randomUUID().toString
+        transactionRepository.create(initialTransaction)
 
-          val transaction = WorkflowTransactionFactory.build(userId, transactionId, define)
-          val initialTransaction = transaction.copy(isInit = true)
-
-          transactionRepository.create(initialTransaction)
-
-        case None =>
-          throw new RuntimeException(s"there is no initial step for workflowId: ${workflowId}")
-      }
-
-    } match {
-      case Success(result)    => result
-      case Failure(exception) => Left(new Exception(exception))
+      case None =>
+        Left(
+          new RuntimeException(
+            s"there is no initial step for workflowId: ${workflowId}"))
     }
   }
 
