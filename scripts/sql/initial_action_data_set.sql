749ee18b786b5cc5ff2a74aacab4acee37ca3b37
letusfly85 <letusfly95@gmail.com>
2018-05-02 16:22:15 +0900

    feat: add initial data set for craft line actions

---
+++ scripts/sql/initial_action_data_set.sql
@@ -0,0 +1,13 @@
+delete from craft_line_actions;
+
+insert into craft_line_actions (id, name, service_id) values (1, '設計書を保存する', 1);
+insert into craft_line_actions (id, name, service_id) values (2, '材料を調達する', 1);
+insert into craft_line_actions (id, name, service_id) values (4, '組み立てる', 1);
+insert into craft_line_actions (id, name, service_id) values (5, 'ヤスリをかける', 1);
+insert into craft_line_actions (id, name, service_id) values (6, '塗装する', 1);
+insert into craft_line_actions (id, name, service_id) values (7, 'ニスを塗る', 1);
+insert into craft_line_actions (id, name, service_id) values (8, 'シールを貼る', 1);
+insert into craft_line_actions (id, name, service_id) values (9, '梱包する', 1);
+insert into craft_line_actions (id, name, service_id) values (10, '出荷依頼をする', 1);
+
+
