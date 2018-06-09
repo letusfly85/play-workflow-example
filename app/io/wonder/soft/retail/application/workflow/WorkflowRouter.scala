846f5af8e82bcab9de32791e5697bc5fcbfa6e87
letusfly85 <letusfly95@gmail.com>
2018-06-09 16:47:10 +0900

    feat: use Router for workflow controllers

--- app/io/wonder/soft/retail/application/workflow/WorkflowRouter.scala
+++ app/io/wonder/soft/retail/application/workflow/WorkflowRouter.scala
@@ -7,28 +7,48 @@ import play.api.routing.Router.Routes
 import play.api.routing.SimpleRouter
 import play.api.routing.sird._
 
-class WorkflowRouter @Inject() (workflowController: WorkflowController, workflowTransactionController: WorkflowTransactionController) extends SimpleRouter {
+class WorkflowRouter @Inject()
+  (workflowController: WorkflowController,
+   workflowTransactionController: WorkflowTransactionController) extends SimpleRouter {
 
   val prefix: String = "/api/workflow"
 
   override def routes: Routes = {
-    case GET(p"/workflow-statuses") =>
+    case GET(p"/statuses") =>
       workflowController.listStatus
 
-    case POST(p"/workflow-statuses") =>
+    case POST(p"/statuses") =>
       workflowController.createStatus
 
-    case PUT(p"/workflow-statuses") =>
+    case PUT(p"/statuses") =>
       workflowController.updateStatus
 
-    case GET(p"/workflow-summaries") =>
+    case GET(p"/summaries") =>
       workflowController.listSummary
 
-    case POST(p"/workflow-summaries") =>
+    case POST(p"/summaries") =>
       workflowController.createSummary
 
-    case PUT(p"/workflow-statuses") =>
-      workflowController.updateStatus
+    case GET(p"/definitions") =>
+      workflowController.listDefinition
+
+    case GET(p"/definitions/$id") =>
+      workflowController.findDefinition(id)
+
+    case POST(p"/definitions") =>
+      workflowController.createDefinition
+
+    case GET(p"/transitions") =>
+      workflowController.listTransition
+
+    case POST(p"/transitions") =>
+      workflowController.createTransition
+
+    case GET(p"/user-transitions") =>
+      workflowTransactionController.listTransition
+
+    case POST(p"/user-transactions") =>
+      workflowTransactionController.proceedState
   }
 
 }
