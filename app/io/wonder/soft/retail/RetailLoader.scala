c4477f053533dcf4af70936e9ee23c1f0295918d
letusfly85 <letusfly95@gmail.com>
2018-06-09 12:04:35 +0900

    feat: comment out other controllers

--- app/io/wonder/soft/retail/RetailLoader.scala
+++ app/io/wonder/soft/retail/RetailLoader.scala
@@ -1,14 +1,13 @@
 package io.wonder.soft.retail
 
-import io.wonder.soft.retail.application.workflow.WorkflowServicesModule
 import play.filters.HttpFiltersComponents
-
 import com.softwaremill.macwire._
 import play.api.ApplicationLoader.Context
 import play.api._
 import play.api.mvc._
 import play.api.routing.Router
 import router.Routes
+import scalikejdbc.config.DBs
 
 class RetailLoader extends ApplicationLoader {
   def load(context: Context) = {
@@ -18,7 +17,7 @@ class RetailLoader extends ApplicationLoader {
 
 class RetailComponents(context: Context)
   extends BuiltInComponentsFromContext(context)
-  with WorkflowServicesModule
+  with RetailModule
   with HttpFiltersComponents {
 
   // set up logger
@@ -26,6 +25,7 @@ class RetailComponents(context: Context)
     _.configure(context.environment, context.initialConfiguration, Map.empty)
   }
 
+  DBs.setupAll()
 
   lazy val router: Router = {
     // add the prefix string in local scope for the Routes constructor
