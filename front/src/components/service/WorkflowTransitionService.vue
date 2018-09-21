4af74f8447443f7ca887140f39daeb9d5eecc11d
letusfly85 <letusfly95@gmail.com>
2018-09-22 07:47:05 +0900

    feat: add workflow transition API service

---
+++ front/src/components/service/WorkflowTransitionService.vue
@@ -0,0 +1,39 @@
+<script>
+import ApiClient from '../utils/ApiClient'
+
+export default {
+  name: 'WorkflowTransitionService',
+  list: function (workflowId, callback, errorHandler) {
+    let targetPath = `/api/workflows/${workflowId}/transitions`
+    ApiClient.search(targetPath, (response) => {
+      callback(response)
+    }, (error) => {
+      errorHandler(error)
+    })
+  },
+  find: function (id, workflowId, callback, errorHandler) {
+    let targetPath = `/api/workflows/${workflowId}/transitions/${id}`
+    ApiClient.find(targetPath, id, (response) => {
+      callback(response)
+    }, (error) => {
+      errorHandler(error)
+    })
+  },
+  create: function (workflowId, params, callback, errorHandler) {
+    let targetPath = `/api/workflows/${workflowId}/transitions`
+    ApiClient.create(targetPath, params, (response) => {
+      callback(response)
+    }, (error) => {
+      errorHandler(error)
+    })
+  },
+  destroy: function (id, workflowId, params, callback, errorHandler) {
+    let targetPath = `/api/workflows/${workflowId}/transitions/${id}`
+    ApiClient.delete(targetPath, params, (response) => {
+      callback(response)
+    }, (error) => {
+      errorHandler(error)
+    })
+  }
+}
+</script>
